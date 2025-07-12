ed6c0866665ba80dc9dbd74496a190fa7a821742
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-07-13 01:38:12 +0300

    feat: init capacitor app

---
+++ apps/client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.familymanager.app',
+  appName: 'Family manager',
+  webDir: 'dist'
+};
+
+export default config;
