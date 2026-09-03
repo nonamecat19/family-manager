5e9496a7143f0a294794b0b69f5df18aa8ef6777
Shumskyi Oleksanrd <shumsky.alexander.work@gmail.com>
2026-09-03 20:33:20 +0300

    feat(notes-java): add the spring boot entrypoint

---
+++ services/notes-java/src/main/java/com/example/notesjava/NotesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesJavaApplication.class, args);
+	}
+
+}
