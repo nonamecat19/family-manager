a146f5324475bfad19e9b34e72acbb1559fcd369
Shumskyi Oleksanrd <shumsky.alexander.work@gmail.com>
2026-09-13 19:14:23 +0300

    test(notes-java): add the context load smoke test

---
+++ services/notes-java/src/test/java/com/example/notesjava/NotesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesjava;
+
+import com.example.notesjava.support.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesJavaApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
